04a6ece78c7d0956167839eaf822d12012452292
Zaw Mai <z.mai@fi.com>
2021-07-18 23:34:46 -0700

    Refactored Gallery and ProductCard components

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent';
