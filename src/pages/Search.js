743ab0c086ccb29a3d9304ef7cd2e3bae358f888
Zaw Mai <z.mai@fi.com>
2021-07-18 23:33:03 -0700

    Create Pages directory and Search page

---
+++ src/pages/Search.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import MockProducts from '../mockData.json';
+import ProductCard from '../components/ProductCard';
+import Gallery from '../components/Gallery';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    padding: '40px 20px 40px 20px'
+  }
+});
+
+function Search(props) {
+  const [products, setProducts] = useState(MockProducts.data);
+  const productCards = products.map((product) => 
+    <ProductCard product={product}/>
+  );
+
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Gallery>
+        {productCards}
+      </Gallery>
+    </div>
+  );
+}
+
+export default Search;
