50add4a264918f598929986d7b13520265588cab
Zaw Mai <z.mai@fi.com>
2021-07-18 23:34:00 -0700

    Added Search page-component to App component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import { useState } from 'react';
-import MockProducts from './mockData.json';
-import * as React from 'react';
-import Grid from '@material-ui/core/Grid';
-import ProductCard from './components/ProductCard';
-import Gallery from './components/Gallery';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  root: {
-    padding: '40px 20px 40px 20px'
-  }
-});
-
+import { React } from 'react';
+import Search from './pages/Search';
 
 function App() {
-
-  const [products, setProducts] = useState(MockProducts.data);
-  const productCards = products.map((product) => 
-    <ProductCard product={product}/>
-  );
-
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <Gallery>
-        {productCards}
-      </Gallery>
-    </div>
+    <Search />
   );
 }
 
