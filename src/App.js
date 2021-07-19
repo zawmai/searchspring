a7c61db62294dd5de370e00a67cb7d54d78992d6
Zaw Mai <z.mai@fi.com>
2021-07-18 17:25:30 -0700

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Products from './products.json';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 
 function App() {
+
+  function ProductItem(props) {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper elevation={3}>{props.data.name}</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  const [products, setProducts] = useState(Products.data);
+  const productCards = products.map((product) => 
+    <ProductItem data={product}/>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container justifyContent="center" spacing={2} className="App">
+        {productCards}
+    </Grid>
   );
 }
 
