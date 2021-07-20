fbc3900c32ca46d8d8379948c9a26d5fbf011fab
Zaw Mai <z.mai@fi.com>
2021-07-19 17:16:12 -0700

    Updated styling for SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,17 +4,27 @@ import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles({
+  searchTextInput: {
+    maxWidth: 600,
+    minWidth: 260,
+    width: '70vw',
 
-
-
+  },
+  searchButton: {
+    margin: '0 0 0 20px',
+    padding: '12px 16px 12px 16px',
+  }
+});
 
 function SearchBar(props) {
+  const classes = useStyles();
   return (
-    <Grid container alignItems="center" justifyContent="center">
+    <Grid className={props.className} container alignItems="center" justifyContent="center">
       <form noValidate autoComplete="off">
         <Grid container alignItems="center" justifyContent="center">
-          <TextField required id="searchTextField" defaultValue="Search for brands, category, color, price here" variant="outlined" />
-          <Button variant="contained" color="primary">Search</Button>
+          <TextField className={classes.searchTextInput} required id="searchTextField" placeholder="Search keywords here" variant="outlined" />
+          <Button className={classes.searchButton} variant="contained" color="primary">Search</Button>
         </Grid>
       </form>
     </Grid>
