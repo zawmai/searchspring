4272713139046545ffed2f8918806a3a1a4c8981
Zaw Mai <z.mai@fi.com>
2021-07-22 01:16:57 -0700

    Added search feature

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button';
-
 import InputBase from '@material-ui/core/InputBase';
+import Button from '@material-ui/core/Button';
+import { useForm } from 'react-hook-form';
 
 const useStyles = makeStyles({
   root: {
@@ -17,6 +17,9 @@ const useStyles = makeStyles({
   input: {
     flex: 1,
   },
+  errorMessage: {
+    color: 'red'
+  },
   submit: {
     borderTopLeftRadius: 0,
     borderBottomLeftRadius: 0,
@@ -24,12 +27,36 @@ const useStyles = makeStyles({
 });
 
 function SearchBar(props) {
+  const { 
+    register,
+    handleSubmit,
+    clearErrors,
+    formState: { errors },
+  } = useForm({ reValidateMode: "onSubmit" });
+
+
+  const inputOptions = {
+    required: 'Required: Please enter one or more keywords to search.',
+    pattern: {
+      value: /^[A-Za-z0-9]+[A-Za-z0-9\s]*$/,
+      message: 'Invalid Keywords(s): Letters and Digits only'
+    }
+  };
+  const searchText = register("searchText", inputOptions);
+  const onChangeCb = (event) => {
+    searchText.onChange(event);
+    clearErrors('searchText');
+  };
+
   const classes = useStyles();
   return (
-    <Paper className={classes.root} component="form" elevation={3} noValidate autoComplete="off">
-      <InputBase className={classes.input} required id="searchTextField" placeholder="Search Product Keywords" variant="outlined" inputProps={{ 'aria-label': 'search product keywords' }} />
-      <Button className={classes.submit} variant="contained" color="primary" type="submit" name="search_text_button" aria-label="search">Search</Button>
-    </Paper>
+    <React.Fragment>
+      <Paper className={classes.root} component="form" elevation={3} noValidate autoComplete="off" onSubmit={handleSubmit(props.onValidSubmit, props.onInvalidSubmit)}>
+        <InputBase  {...searchText} onChange={onChangeCb} className={classes.input} id="searchTextField" name="searchText" placeholder="Search Product Keywords" variant="outlined" inputProps={{ 'aria-label': 'search product keywords' }} />
+        <Button className={classes.submit} variant="contained" color="primary" type="submit" name="searchButton" aria-label="search">Search</Button>
+      </Paper>
+      {errors.searchText && errors.searchText.message && <p className={classes.errorMessage}>{errors.searchText.message}</p>}
+    </React.Fragment>
   );
 }
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,6 +5,8 @@ import SearchBar from '../components/SearchBar';
 import Section from '../components/Section';
 import ProductCard from '../components/ProductCard';
 import Container from '@material-ui/core/Container';
+import { useEffect } from 'react';
+
 
 const useStyles = makeStyles({
   root: {
@@ -15,18 +17,38 @@ const useStyles = makeStyles({
 function Search(props) {
 
   // Create ProductCard components with data
-  const [products, setProducts] = useState(MockProducts.data);
-  const productCards = products.map((product) => 
-    <React.Fragment key={product.id}>
-      <ProductCard product={product}/>
-    </React.Fragment>
-  );
+  const [data, setData] = useState();
+
+  const handleSubmit = (inputs, event) => {
+    var  siteId = "scmq7n";
+    const base = "https://" + siteId + ".a.searchspring.io/api/search/search.json?";
+    const params = {
+      "siteId": siteId,
+      "resultsFormat": "native",
+      "q": inputs.searchText
+    } 
+
+    var reqUrl = base + (new URLSearchParams(params)).toString();
+    fetch(reqUrl)
+      .then(res => res.json())
+      .then((jsonData) => {
+        setData(jsonData);
+        console.log(jsonData);
+      });
+    console.log(inputs);
+  };
 
   const classes = useStyles();
+  const productCards = (data && data.results && (data.results.length > 0)) 
+    ? (data.results.map((product) => 
+          <React.Fragment key={product.id}>
+            <ProductCard product={product}/>
+          </React.Fragment>))
+    : <p>No Products Found. Please search different keywords =).</p>;
   return (
     <Container maxWidth="md" className={classes.root}>
       <Section>
-        <SearchBar />
+        <SearchBar onValidSubmit={handleSubmit} onInvalidSubmit={null} />
       </Section>
       <Section>
         {productCards}
