4702cb4592eda2adaee1fcd5a0c993e2e6a3eea5
Zaw Mai <10764631+zawmai@users.noreply.github.com>
2022-07-02 01:28:03 -0700

    SearchBar Component: Refactored to use emotion styled component and mui 5.8.6

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,45 @@
 import * as React from 'react';
-import { makeStyles } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper'
-import InputBase from '@material-ui/core/InputBase';
-import Button from '@material-ui/core/Button';
+import Paper from '@mui/material/Paper'
+import InputBase from '@mui/material/InputBase';
+import Button from '@mui/material/Button';
 import { useForm } from 'react-hook-form';
+import { styled } from '@mui/system'
 
-/** MaterialUI style hook to make CSS rules object */
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 0 0 10px',
-    maxWidth: 500,
-    minWidth: 260,
-    width: '100%',
-  },
-  input: {
-    flex: 1,
-  },
-  errorMessage: {
-    flexBasis: '100%',
-    textAlign: 'center',
-    color: 'red'
-  },
-  submit: {
-    borderTopLeftRadius: 0,
-    borderBottomLeftRadius: 0,
-  }
+/** Make styled component w/ emotion styling engine to insert CSS rules */
+const StyledPaper = styled(Paper, {
+  name: "searchBar__form",
+  slot: "wrapper"
+})({
+  display: "flex",
+  alignItems: "center",
+  padding: "0 0 0 10px",
+  maxWidth: 500,
+  minWidth: 260,
+  width: "100%",
+});
+
+const StyledInputBase = styled(InputBase, {
+  name: "searchBar__InputContainer",
+  slot: "wrapper"
+})({
+  flex: 1
+});
+
+const StyledButton = styled(Button, {
+  name: "searchBar__BtnSubmit",
+  slot: "wrapper"
+})({
+  borderTopLeftRadius: 0,
+  borderBottomLeftRadius: 0,
+})
+
+const StyledParagrah = styled('p', {
+  name: "searchBar__txtError",
+  slot: "wrapper"
+})({
+  flexBasis: '100%',
+  textAlign: 'center',
+  color: 'red'
 });
 
 
@@ -75,32 +88,27 @@ function SearchBar(props) {
     clearErrors('searchText');
   };
 
-  /** Create CSS style strings with MaterialUI CSS rules */
-  const classes = useStyles();
-
   return (
     <React.Fragment>
-      <Paper className={classes.root} component="form" elevation={3} noValidate autoComplete="off" onSubmit={handleSubmit(props.onValidSubmit, props.onInvalidSubmit)}>
-        <InputBase  
+      <StyledPaper component="form" elevation={3} noValidate autoComplete="off" onSubmit={handleSubmit(props.onValidSubmit, props.onInvalidSubmit)}>
+        <StyledInputBase  
           {...searchText} 
-          onChange={onChangeCb} 
-          className={classes.input} 
-          id="searchTextField" 
-          name="searchText" 
-          placeholder="Search Product Keywords" 
-          variant="outlined" 
-          inputProps={{ 'aria-label': 'search product keywords' }} 
+          id="searchTextField"
+          onChange={onChangeCb}
+          name="searchText"
+          placeholder="Search Product Keywords"
+          variant="outlined"
+          inputProps={{ 'aria-label': 'search product keywords' }}
         />
-        <Button 
-          className={classes.submit} 
+        <StyledButton  
           variant="contained" 
           color="primary" 
           type="submit" 
           name="searchButton" 
           aria-label="search">Search
-        </Button>
-      </Paper>
-      {errors.searchText && errors.searchText.message && <p className={classes.errorMessage}>{errors.searchText.message}</p>}
+        </StyledButton>
+      </StyledPaper>
+      {errors.searchText && errors.searchText.message && <StyledParagrah>{errors.searchText.message}</StyledParagrah>}
     </React.Fragment>
   );
 }
