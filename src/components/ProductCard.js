3200b9dbf267c20808e725fef36bcfe9943abf15
Zaw Mai <10764631+zawmai@users.noreply.github.com>
2022-07-03 00:53:30 -0700

    ProductCard Component: Refactored to use emotion styled component and mui 5.8.6

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,37 +1,54 @@
 import * as React from 'react';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia'
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/system';
 
-/** MaterialUI style hook to make CSS rules object */
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 267,
-    fontSize: 16,
-    margin: '0 0 30px 0',
-  },
-  media: {
-    width: 267,
-    height: 400,
-  },
-  name: {
-    margin: '0 0 10px 0',
-  },
-  price: {
-    padding: '0 0 0 10px',
-    fontSize: 16,
-    fontWeight: 700,
-    color: '#3a23ad'
-  },
-  msrp: {
-    textDecoration: 'line-through',
-    fontSize: 16,
-  }
+
+/** Make styled component w/ emotion styling engine to insert CSS rules */
+const StyledCard = styled(Card, {
+  name: "productCard__container",
+  slot: "wrapper"
+})({
+  maxWidth: 267,
+  fontSize: 16,
+  margin: '0 0 30px 0',
 });
 
+const StyledCardMedia = styled(CardMedia, {
+  name: "productCard__image",
+  slot: "wrapper"
+})({
+  width: 267,
+  height: 400,
+});
+
+const StyledName = styled(Typography,{
+  name: "productCard__name",
+  slot: "wrapper"
+})({
+  margin: '0 0 10px 0',
+});
+
+const StyledPrice = styled('span',{
+  name: "productCard__price",
+  slot: "wrapper"
+})({
+  padding: '0 0 0 10px',
+  fontSize: 16,
+  fontWeight: 700,
+  color: '#3a23ad'
+});
+
+const StyledMsrp = styled('span',{
+  name: "productCard__msrp",
+  slot: "wrapper"
+})({
+  textDecoration: 'line-through',
+  fontSize: 16,
+});
 
 /**
  * Card component containing image, text, price and msrp.
@@ -47,8 +64,6 @@ const useStyles = makeStyles({
  * @returns React JSX Component
  */
 function ProductCard(props) {
-  /** Create CSS style strings with MaterialUI CSS rules */
-  const classes = useStyles();
 
   /** Destructure product's name and image URL from props */
   const {product: { name, thumbnailImageUrl } } = props;
@@ -60,26 +75,25 @@ function ProductCard(props) {
   // Display msrp crossed out if msrp is greater than price
   // else only display price
   const priceBoxContent = (msrp && price && (msrp > price) 
-    ? <React.Fragment><span className={classes.msrp}>${msrp}</span><span className={classes.price}>${price}</span></React.Fragment>
-    : <span>${price}</span>
+    ? <React.Fragment><StyledMsrp>${msrp}</StyledMsrp><StyledPrice>${price}</StyledPrice></React.Fragment>
+    : <StyledPrice>${price}</StyledPrice>
   );
 
   return (
-    <Card className={classes.root} elevation={3} variant='outlined'>
-      <CardMedia
-        className={classes.media}
+    <StyledCard elevation={3} variant='outlined'>
+      <StyledCardMedia
         image={thumbnailImageUrl}
         title={name}
       />
       <CardContent>
-          <Typography className={classes.name} variant="body1" align="center" color="textPrimary" component="p">
+          <StyledName variant="body1" align="center" color="textPrimary" component="p">
             {name}
-          </Typography>
+          </StyledName>
         <Box display="flex" justifyContent="center">
           {priceBoxContent}
         </Box>
       </CardContent>
-    </Card>
+    </StyledCard>
   );
 }
 
